ab3c8c21023c6daeb1b1bd6fb4f5f128c40cb3fa
laurenz <laurenz.klipper@buspaket>
2021-06-26 00:26:40 +0200

    Fix bad curl parsing (see jr-k fork)

--- src/Webflow/Api.php
+++ src/Webflow/Api.php
@@ -55,6 +55,9 @@ class Api
         $response = curl_exec($curl);
         curl_close($curl);
         list($headers, $body) = explode("\r\n\r\n", $response, 2);
+        while (strpos($body, "\r\n\r\n") !== false) {
+            list($headers, $body) = explode("\r\n\r\n", $body, 2);
+        }
         return $this->parse($body);
     }
     private function get($path)
